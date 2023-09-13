f8d89d61b9378a16ffd6c674cd8b3e850933d65b
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-13 13:47:52 +0200

    promise chaining

--- promises+fetch/promiseChaining.js
+++ promises+fetch/promiseChaining.js
@@ -38,65 +38,34 @@
 //         console.log(val)
 //     })
 
-const stocks = {
-    fruits: ["strawberry", "grapes", "banana", "apple",],
-    liquid: ["water", "ice"],
-    holder: ["cone", "cup", "stick"],
-    toppings: ["chocolate", "peanuts"]
+// 
+
+
+
+
+const job1 = () => {
+    return new Promise((resolve, reject) => {
+        resolve("results of Job 1")
+    });
 }
 
-let is_shop_open = true;
+const job2 = () => {
+    return new Promise((resolve, reject)=> {
+        resolve("results of job 2")
+    });
+}
 
-let order = (time, work) => {
-    return new Promise((resolve, reject)=>{
-        if(is_shop_open) {
-            setTimeout(() => {
-                resolve(work())
-            }, time);
-        } else {
-            reject(console.log("our shop is closed"))
-        }
+const promise = job1();
+
+promise
+    .then((data1) => {
+        console.log("data 1: " + data1)
+        return job2();
     })
-}
-
-order(2000, ()=>console.log(`${stocks.fruits[0]} was selected`))
-
-.then(()=> {
-    return order(0, ()=> console.log("production has started"))
-})
-
-.then(()=> {
-    return order(2000, ()=> console.log(`the fruits has been chopped`))
-})
-
-.then(()=> {
-    return order(1000, ()=> console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`))
-})
-
-.then(()=> {
-    return order(1000, ()=> console.log(`start the machine`))
-})
-
-.then(()=> {
-    return order(2000, ()=> console.log(`Icecream placed on the ${stocks.holder[0]}`))
-})
-
-.then(()=> {
-    return order(3000, ()=> console.log(`${stocks.toppings[0]} was selected`))
-})
-
-.then(()=> {
-    return order(2000, ()=> console.log("Icecream served"))
-})
-
-.catch(()=> {
-   setTimeout(() => {
-    console.log("yallaaah the customer left")
-   }, 2000);
-})
-
-.finally(()=> {
-    setTimeout(() => {
-        console.log("the shop closed")
-    }, 6000);
-})
+    .then((data2) => {
+        console.log("data 2: " + data2)
+        return "Hello world";
+    })
+    .then((data3) => {
+        console.log("data 3: " + data3)
+    })
